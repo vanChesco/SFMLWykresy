7f0f6c7c06592afc06e59c95b487108405a9753c
Dariusz <vanchesco@icloud.com>
2020-06-08 23:14:04 +0200

    Wyswietlanie statusu w konsoli

--- sfml_app.cpp
+++ sfml_app.cpp
@@ -4,48 +4,47 @@
 
 int main()
 {
-	sf::VideoMode::getDesktopMode();
+	const int resX{ 1920 }, resY{ 1200 };																			// Deklaracja rozdzielczosci
 
-	const int resX{ 1920 }, resY{ 1200 };																// Deklaracja rozdzielczosci
+	sf::ContextSettings settings;																					// Ustawienie antyaliasingu
+		settings.antialiasingLevel = 16;
+	
+	float margX{ 20 }, margY{ 20 };																					// Marginesy
 
-	float margX{ 20 }, margY{ 20 };																		// Marginesy
+	const float M_PI = (float)acos(-1);																				// Definicja stalej PI
+	float x, y;																										// Zmienne do obliczania wykresu
+	int scaleY{ 2 }, scaleX{ 1 };																					// Zmienne do skalowania
+	int iloscPodzialekX{ 6 }, iloscPodzialekY{ 6 };																	// Liczba podzialek na osi X
+	float uX{ 1.0f * M_PI }, uY{ 2 };																				// Jednostki osi
+	float x_max{ 4.f * M_PI }, y_max{ 2.f };																		// Wartosci mx osi
+	float x_min{ -0.1f * M_PI }, y_min{ -2.f };																		// Wartosci mx osi
 
-
-	const float M_PI = (float)acos(-1);																	// Definicja stalej PI
-	float x, y;																							// Zmienne do obliczania wykresu
-	int scaleY{ 1 }, scaleX{ 1 };																		// Zmienne do skalowania
-	int iloscPodzialekX{ 6 }, iloscPodzialekY{ 6 };														// Liczba podzialek na osi X
-	float uX{ 0.5f * M_PI }, uY{ 1 };																	// Jednostki osi
-	float x_max{ 4.f * M_PI }, y_max{ 1.5 };																// Wartosci mx osi
-	float x_min{ -0.3f * M_PI }, y_min{ -1.5 };																// Wartosci mx osi
-
-	float dX = (resX - margX) / (x_max - x_min);
-	float dY = (resY - margY) / (y_max - y_min);														// Dlugosc jednej dzialki
+	float dX = (resX - 2 * margX) / (x_max - x_min);				
+	float dY = (resY - 2 * margY) / (y_max - y_min);																// Dlugosc jednej dzialki
 
 	float xo = dX * (-x_min);
 	float yo = dY * (-y_min);
 
-	sf::Vector2f axisOrigin{ xo, yo };																	// Punkt skrzyzowania osi
+	sf::Vector2f axisOrigin{ xo, yo };																				// Punkt skrzyzowania osi
 
-	bool pokaz_kolko{ false };																			// Przelacznik widocznosci kolka
+	bool pokaz_kolko{ false };																						// Przelacznik widocznosci kolka
+
+
+
 
 	sf::Font czcionka;
-	czcionka.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
+		czcionka.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
+	
 
 
-
-
-
-
-
-	sf::RenderWindow window(sf::VideoMode(resX, resY, 32), "SFML Wykresy", sf::Style::Fullscreen);		// Utworzenie okna programu
-		window.setMouseCursorVisible(false);															// Wylaczenie kursora
-		window.setFramerateLimit(25);																	// Czestotliwosc odswiezania
-		window.setVerticalSyncEnabled(true);															// Vertical Sync On
+	sf::RenderWindow window(sf::VideoMode(resX, resY, 32), "SFML Wykresy", sf::Style::Fullscreen, settings);		// Utworzenie okna programu
+		window.setMouseCursorVisible(false);																		// Wylaczenie kursora
+		window.setFramerateLimit(120);																				// Czestotliwosc odswiezania
+		window.setVerticalSyncEnabled(true);																		// Vertical Sync On
 
 	sf::CircleShape kolko(10); 
-	kolko.setPosition(1890, 0);
-	kolko.setFillColor(sf::Color::Green);
+		kolko.setPosition(1890, 0);
+		kolko.setFillColor(sf::Color::Green);
 
 	sf::CircleShape* punkty = new sf::CircleShape[resX]; 
 	sf::CircleShape* xy = punkty;
@@ -72,14 +71,15 @@ int main()
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Multiply) scaleX+=10;
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Divide) scaleX-=10;
 
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S) axisOrigin.y+=10;
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::W) axisOrigin.y-=10; 
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D) axisOrigin.x+=10;
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A) axisOrigin.x-=10;
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S && event.key.code == sf::Keyboard::LShift) axisOrigin.y += 100.f;			// ???????????????????????
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::W && event.key.code == sf::Keyboard::LShift) axisOrigin.y -= 100.f;			// ???????????????????????
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D && event.key.code == sf::Keyboard::LShift) axisOrigin.x += 100.f;			// ???????????????????????
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A && event.key.code == sf::Keyboard::LShift) axisOrigin.x -= 100.f;			// ???????????????????????
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad8) axisOrigin.y+=10;
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad2) axisOrigin.y-=10; 
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad6) axisOrigin.x+=10;
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad4) axisOrigin.x-=10;
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad8 && event.key.code == sf::Keyboard::LShift) axisOrigin.y += 100.f;			// ???????????????????????
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad2 && event.key.code == sf::Keyboard::LShift) axisOrigin.y -= 100.f;			// ???????????????????????
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad6 && event.key.code == sf::Keyboard::LShift) axisOrigin.x += 100.f;			// ???????????????????????
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Numpad4 && event.key.code == sf::Keyboard::LShift) axisOrigin.x -= 100.f;			// ???????????????????????
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) std::cout << 1;
 		}
 
 		window.clear(sf::Color::Black);
@@ -99,35 +99,32 @@ int main()
 		}
 
 
-		sf::VertexArray osX(sf::Lines, 2);																	// Linia osi X
-		osX[0].position = sf::Vector2f(margX, axisOrigin.y);												// 
-		osX[1].position = sf::Vector2f(float(resX) - margX, axisOrigin.y);									// 
+		sf::VertexArray osX(sf::Lines, 2);																		// Linia osi X
+		osX[0].position = sf::Vector2f(margX, axisOrigin.y);													// 
+		osX[1].position = sf::Vector2f(float(resX) - margX, axisOrigin.y);										// 
 
-		sf::VertexArray osY(sf::Lines, 2);																	// Linia osi Y
-		osY[0].position = sf::Vector2f(axisOrigin.x, margY);												// 
-		osY[1].position = sf::Vector2f(axisOrigin.x, float(resY) - margY);									// 
+		sf::VertexArray osY(sf::Lines, 2);																		// Linia osi Y
+		osY[0].position = sf::Vector2f(axisOrigin.x, margY);													// 
+		osY[1].position = sf::Vector2f(axisOrigin.x, float(resY) - margY);										// 
 
-		sf::VertexArray grotX(sf::Triangles, 3);															// Grot osi X
-		grotX[0].position = sf::Vector2f(float(resX) - margX, axisOrigin.y);								// 
-		grotX[1].position = sf::Vector2f(float(resX) - margX - 20.f, axisOrigin.y - 4.f);					// 
-		grotX[2].position = sf::Vector2f(float(resX) - margX - 20.f, axisOrigin.y + 4.f);					// 
+		sf::VertexArray grotX(sf::Triangles, 3);																// Grot osi X
+		grotX[0].position = sf::Vector2f(float(resX) - margX, axisOrigin.y);									// 
+		grotX[1].position = sf::Vector2f(float(resX) - margX - 20.f, axisOrigin.y - 4.f);						// 
+		grotX[2].position = sf::Vector2f(float(resX) - margX - 20.f, axisOrigin.y + 4.f);						// 
 
-		sf::VertexArray grotY(sf::Triangles, 3);															// Grot osi Y
-		grotY[0].position = sf::Vector2f(axisOrigin.x, margY);												// 
-		grotY[1].position = sf::Vector2f(axisOrigin.x - 4.f, margY + 20.f);									// 
-		grotY[2].position = sf::Vector2f(axisOrigin.x + 4.f, margY + 20.f);									// 
+		sf::VertexArray grotY(sf::Triangles, 3);																// Grot osi Y
+		grotY[0].position = sf::Vector2f(axisOrigin.x, margY);													// 
+		grotY[1].position = sf::Vector2f(axisOrigin.x - 4.f, margY + 20.f);										// 
+		grotY[2].position = sf::Vector2f(axisOrigin.x + 4.f, margY + 20.f);										// 
 
 
-		dX = (resX - 2 * margX + scaleX) * uX / (x_max); //std::cout << dX << '\n';
-
-		int xpmin = int(-axisOrigin.x/dX);																	// ilosc podzialek na minusie
+		dX = (resX - 2 * margX + scaleX) * uX / (x_max);														//todo Gdzies tu dochodzi do dzielenia przez 0
+		int xpmin = int(-axisOrigin.x/dX);																		// ilosc podzialek na minusie
 		int xpmax = int((resX - margX - axisOrigin.x) / dX);
 		iloscPodzialekX = xpmax - xpmin;
-		//std::cout << iloscPodzialekX << " | " << xpmin << " | " << xpmax << '\n';
 
 		dY = (resY - 2 * margY + scaleY) * uY / (y_max- y_min); 
-
-		int ypmin = -1 * int((resY - margY - axisOrigin.y )/ (int)dY);										// ilosc podzialek na minusie
+		int ypmin = -1 * int((resY - margY - axisOrigin.y )/ (int)dY);											// ilosc podzialek na minusie
 		int ypmax = int((axisOrigin.y - margY) / dY);
 		iloscPodzialekY = ypmax - ypmin;
 	
@@ -181,11 +178,13 @@ int main()
 				ypmax--;
 			}
 
-
+			int fmax = 20;
 		for (int i = 0; i < resX; i++)																			// Rysowanie wykresu punkt po punkcie
 		{
-			x = 2.f * M_PI * (float(i) - axisOrigin.x) / ((float(resX) - 2* margX) + (float)scaleX);
-			y = sin(x * sin(2 * x));																				// Formula do wyswietlenia
+			x = 4.f * M_PI * (float(i) - axisOrigin.x) / ((float(resX) - 2* margX) + (float)scaleX);
+			y = sin(x);
+			
+			//y = y = sin((2 * M_PI / ((1 - 1 / fmax) * x + 2 * M_PI / fmax)) * x) * (2 * M_PI / ((1 - 1 / fmax) * x + 2 * M_PI / fmax)) / fmax;				// Formula do wyswietlenia
 
 			(xy + i) -> setRadius(1);
 			(xy + i) -> setFillColor(sf::Color::Red);
@@ -194,6 +193,10 @@ int main()
 		}
 
 		window.display();
+
+		std::cout << "Przeciecie ukladu wspolrzednych" << "\t\t" << '\n';
+		std::cout << "x: " << axisOrigin.x << "\ty: " << axisOrigin.y << '\n';
+		std::cout << "Skala X: " << scaleX << ", Skala Y: " << scaleY << '\n';
 
 		delete[] podzX;
 		delete[] podzY;
